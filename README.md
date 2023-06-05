90fc6c148537a6ac42ceda0291e61dc22ee163d9
Noah Shinn <noahshinn024@Noahs-MBP.attlocal.net>
2023-06-04 17:10:53 -0700

    add copy to clipboard, command explanation, and gpt chat

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ git clone https://github.com/noahshinn024/bashllm
 
 Run `make`:
 ```
-cd ./bashllm && make
+cd ./bashllm && sudo make
 ```
 
 ## To Run
@@ -25,10 +25,47 @@ bashllm <command description>
 ## Examples
 ```
 >>> bashllm "search for all open ports on ip"
-sudo nmap <ip addr>
+
+Command:
+
+    sudo nmap -p- <target_ip>
+
+Explanation:
+
+The -p- option tells nmap to scan all ports, from 1 to 65535. This is useful if you want to scan for all open ports on a target. Replace <target_ip> with the IP address or hostname of the target you want to scan.
+
+Command has been copied to clipboard!
+```
+
+```
+>>> bashllm "copy file to aws s3 bucket"
+
+Command:
+
+    aws s3 cp <local_file_path> s3://<bucket_name>/<destination_path>
+
+Explanation:
+
+Let's break down the options used in this command:
+
+<local_file_path>: Specifies the path of the file you want to copy to the S3 bucket.
+s3://<bucket_name>/<destination_path>: Specifies the destination path in the S3 bucket where you want to copy the file. Replace <bucket_name> with the name of your S3 bucket and <destination_path>
+
+Command has been copied to clipboard!
+
 ```
 
 ```
 >>> bashllm "i use zsh, read from ~/.zsh_history give me the 10 most used commands"
-cat ~/.zsh_history | sort | uniq -c | sort -nr | head -10
+
+Command:
+
+    history | awk '{a[$2]++}END{for(i in a){print a[i] " " i}}' | sort -rn | head -n 10
+
+Explanation:
+
+This command uses the history command to display the command history of the current session. The output is then piped to awk, which counts the frequency of each command and sorts them in descending order. Finally, the head command is used to display only the top 10 most used commands.
+
+Command has been copied to clipboard!
+
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::env;
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
 use reqwest;
 use tokio;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
 struct Prompt {
-    prompt: String,
+    messages: Vec<Message>,
     model: String,
     temperature: f64,
     max_tokens: u32,
@@ -20,31 +22,45 @@ struct Response {
 
 #[derive(Deserialize)]
 struct Choice {
-    text: String,
+    message: Message,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    role: String,
+    content: String
 }
 
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        println!("Usage: bashhelp <prompt>");
+        println!("Usage: bashllm <natural language description>");
         return;
     }
-    let base_prompt = "You are an AI coding assistant that helps the user to write bash commands for penetration testing purposes. You have sudo access. You should read the command or intent message, then print a valid bash command that the user can use. Put the command on the same line as the \">\". For example:\nCommand/intent: scan for open ports using nmap\n> sudo nmap 192.168.0.1\n\nCommand/intent: ";
+    let base_prompt = "You are an AI coding assistant that helps the user to write bash commands for penetration testing purposes. You should read the intent message, then print a valid bash command or command template that the user can use. For example:\nIntent: scan for open ports in a certain range using nmap and determine services or versions\nCommand: sudo nmap -p <port_range> -sV -A <target_ip>\nExplanation:\nLet's break down the options used in this command:\n\n-p <port_range>: Specifies the port range to scan. You can specify a single port or a range of ports. For example, you can use -p 1-100 to scan ports 1 to 100.\n-sV: Performs service/version detection to determine the service running on the open ports.\n-A: Enables aggressive scanning options including OS detection, version detection, script scanning, and traceroute.\n<target_ip>: Replace this with the IP address or hostname of the target you want to scan for open ports.\n\nIntent: ";
     let user_prompt = args[1].clone();
+
     let prompt = Prompt {
-        prompt: format!("{}{}\n>", base_prompt, user_prompt),
-        model: "text-davinci-003".to_string(),
+        model: "gpt-3.5-turbo".to_string(),
+        messages: vec![
+            Message {
+                role: "user".to_string(),
+                content: format!("{}{}\n>", base_prompt, user_prompt)
+            }
+        ],
         temperature: 0.0,
         max_tokens: 100,
         top_p: 1,
-        n: 1,
+        n: 1
     };
 
-    let api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
+
+    let api_key = env::var("OPENAI_API_KEY").or_else(|_| env::var("OPENAI_APIKEY"))
+    .expect("Either OPENAI_API_KEY or OPENAI_APIKEY not set");
     let client = reqwest::Client::new();
     let response = client
-        .post("https://api.openai.com/v1/completions")
+        .post("https://api.openai.com/v1/chat/completions")
         .header("Content-Type", "application/json")
         .header("Authorization", format!("Bearer {}", api_key))
         .json(&prompt)
@@ -54,8 +70,25 @@ async fn main() {
         Ok(res) => {
             if res.status().is_success() {
                 let result: Response = res.json().await.unwrap();
-                let text = result.choices[0].text.trim();
-                println!("{}", text);
+                let response: Vec<&str> = result.choices[0].message.content.trim().split("Command:").collect::<Vec<&str>>().last().unwrap_or(&"").split("Explanation:").collect();
+                let command_text = response.first().unwrap_or(&"").trim();
+                let explanation_text = response.last().unwrap_or(&"").trim();
+                let is_copied: bool;
+                if let Ok(mut ctx) = ClipboardContext::new() {
+                    if let Err(err) = ctx.set_contents(command_text.to_owned()) {
+                        println!("Failed to copy to clipboard: {}", err);
+                        is_copied = false;
+                    } else {
+                        is_copied = true;
+                    }
+                } else {
+                    println!("Failed to access clipboard");
+                    is_copied = false;
+                }
+                println!("\nCommand:\n\n    {}\n\nExplanation:\n\n{}", command_text, explanation_text);
+                if is_copied {
+                    println!("\nCommand has been copied to clipboard!\n");
+                }
             } else {
                 println!("Request failed with status: {}", res.status());
             }
